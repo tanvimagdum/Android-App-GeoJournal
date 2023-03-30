7e36638e60065a4b949d7307dc57a655ac958411
tanvimagdum <magdum.t@northeastern.edu>
2023-03-30 19:11:38 -0400

    commit Display page

---
+++ app/src/main/java/com/example/nu_mad_sp2023_final_project_15/DisplayPage.java
@@ -0,0 +1,14 @@
+package com.example.nu_mad_sp2023_final_project_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_page);
+    }
+}
